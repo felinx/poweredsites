59df26f85b45ed7ec1b4340344f7e8de262b1dba
felinx <devnull@localhost>
2010-08-03 19:38:38 +0800

    fix formencode subdomain regex

--- poweredsites/forms/base.py
+++ poweredsites/forms/base.py
@@ -88,10 +88,10 @@ class URL(validators.URL):
     url_re = re.compile(r'''
         ^(http|https)://
         (?:[%:\w]*@)?                           # authenticator
-        (?P<domain>[a-z0-9][a-z0-9\-]{0,62}* # (sub)domain - alpha followed by 62max chars (63 total)
+        (?P<domain>[a-z0-9][a-z0-9\-]{0,62}\.)* # (sub)domain - alpha followed by 62max chars (63 total)
         (?P<tld>[a-z]{2,})                      # TLD
         (?::[0-9]+)?                            # port
-    
+
         # files/delims/etc
         (?P<path>/[a-z0-9\-\._~:/\?#\[\]@!%\$&\'\(\)\*\+,;=]*)?
         $
